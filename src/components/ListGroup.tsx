aa13f9d9f9aa5c1ad5bd1f85e2c6ee1390b77f84
BishwasKoirala <wasubisu69@gmail.com>
2023-12-15 00:35:25 +0900

    continud

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
 
 function ListGroup() {
+
+  let items = [
+    'New Yord',
+    'Anadifja',
+    'Sillicon Valleu',
+    'Japan',
+    'Londaon',
+  ]
+
+  items = []
+
+  const message = items.length === 0 ? 
+  <p>Not items found</p> : null
+
+
+  const getmessage = () => {
+    return items.length === 0 ?
+      <p>No items Found</p>
+      : null //else null
+  }
+
   return (
+  <>
+  <h1>List</h1>
+  { getmessage }
   <ul className="list-group">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
+    { items.map( item =>
+      <li key={item}>{item}</li> ) }
   </ul>
+  </>
   )
 }
 
