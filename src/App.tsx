49cefeec124825f7ad6a30f7a0f75a0413401693
BishwasKoirala <wasubisu69@gmail.com>
2024-01-22 15:07:59 +0900

    did up to updating nested object , next do =>  updating arrays

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,32 @@ import Message from "./components/Message";
 export default function App () {
   // const [firstName , setFirstName] = useState('');
   // const [lastName , setLastName] = useState('');
+  const [customer , setCustomer] = useState(
+    {
+      name : 'John' , 
+      address : {
+        city : 'Yokohama' ,
+        zip : 2440805
+      }
+    }
+  )
 
+  const handleClick = () => {
+    setCustomer({
+      // ..means all existing objects
 
+      ...customer,
+      address : {
+        ...customer.address,
+        zip : 21111
+      }
+    })
+  }
 
   return (
     <div>
-      <Message/>
-      
+      <button onClick={handleClick} >click</button>
+      <p>{customer.name}</p>  
     </div>
   );
 }
