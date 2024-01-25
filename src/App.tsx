badb04abb8592988c4526fb8014128f9cce8838c
BishwasKoirala <wasubisu69@gmail.com>
2024-01-25 11:17:46 +0900

    next do form lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import React, { useState } from "react";
+import React from 'react'
+import ExpandableText from './components/ExpandableText'
 
 const App = () => {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product1", quantity: 1 },
-      { id: 2, title: "Product2", quantity: 1 },
-      
-  ],
-  });
+  return (
+    <ExpandableText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, culpa. Ullam numquam itaque, blanditiis nemo nostrum fuga. Vel, velit, hic, itaque blanditiis quo repellendus quos libero illo facere quibusdam incidunt.lorem</ExpandableText>
+  )
+}
 
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.quantity === 1 ? { ...item, quantity: 2 } : item
-      ),
-    });
-  };
-
-  return <div>
-    <button onClick={handleClick}>click</button>
-    {cart.items.map(item => <p key={item.id}>{item.quantity}</p>)}
-  </div>;
-};
-
-export default App;
+export default App
