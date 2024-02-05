4144759af33a0d5ce95d3c01e4e6de1cee000e9f
BishwasKoirala <wasubisu69@gmail.com>
2024-02-05 21:49:37 +0900

    できん、かえってもう一回やろっと

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ExpandableText from './components/ExpandableText'
 import Form from './components/Form'
+import Expense from './components/ExpenseTracker/Expense'
 
 const App = () => {
   return (
     <div>
-      
+      <Expense />
     </div>
   )
 }
