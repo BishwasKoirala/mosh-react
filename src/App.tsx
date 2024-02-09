bd6882a8fdc45adfd04e09d6f0ea5045058a1ffd
BishwasKoirala <wasubisu69@gmail.com>
2024-02-09 16:40:54 +0900

    expense tracker ex finished
    nead to learn more about react components Prop and forms handeling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/Expense-Form";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -29,7 +26,14 @@ const App = () => {
     return (
       <div>
         <div className="mb-5">
-          <ExpenseForm />
+          <ExpenseForm
+            onSubmit={(expense) =>
+              setExpenses([
+                ...expenses,
+                { ...expense, id: expenses.length + 1 },
+              ])
+            }
+          />
         </div>
         <div className="mb-3">
           <ExpenseFilter
---
+++ src/expense-tracker/categories.ts
@@ -0,0 +1,4 @@
+const categories = ["Groceries", "Utilities", "Entertainment"] as const
+//as const to make this array read only
+
+export default categories
