0b7e6276727b19e85aec6f7a63aa77985ec32dc6
BishwasKoirala <wasubisu69@gmail.com>
2024-02-11 23:27:30 +0900

    useeffetch => get req using axios and handeling req errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { number } from "zod";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 const App = () => {
-  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => console.log(res.data) )
-  });
-  return <div>App</div>;
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+        setUsers(res.data);
+      })
+      // give a callback function in catch to catch error
+      .catch((anError) => setError(anError.message));
+  }, []);
+  return (
+    <div>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
